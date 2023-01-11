34c69d81ceaca269a1163a5c5333cd4ddf4cec09
unknown <uk2459644@gmail.com>
2023-01-11 08:13:49 +0530

    handle method added to basecommand

--- mockapp/management/commands/createsuper.py
+++ mockapp/management/commands/createsuper.py
@@ -5,9 +5,10 @@ User=get_user_model()
 
 class Command(BaseCommand):
     help='Creates a superuser'
-    if not User.objects.filter(username='example2@gmail.com').exists():
-        User.objects.create_superuser(
-            username='example2@gmail.com',
-            password='Soltn2@5102'
-        )
-    print('Superuser has been created.')
+    def handle(self, *args,**options):
+        if not User.objects.filter(username='example2@gmail.com').exists():
+            User.objects.create_superuser(
+                username='example2@gmail.com',
+                password='Soltn2@5102'
+            )
+        print('Superuser has been created.')
